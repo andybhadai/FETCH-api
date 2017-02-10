dc8b15c93df2e579d03b4782fbf967593da6dc38
Andy Bhadai <abhadai@gmail.com>
2017-02-10 15:31:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,27 @@ const fetch = require('node-fetch');
 fetch('http://swapi.co/api/people/1')
         .then((response) => response.json())
         .then(console.log);
+
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    get Name(){
+        return this.name;
+    }
+
+    get Age(){
+        return this.age;
+    }
+}
+
+let persons = [];
+let printPerson = (person) => {return `This person is called ${person.Name} and his age is ${person.Age}`};
+
+for(let i = 0; i < 10; i ++){
+    persons.push(new Person(`Person ${i}`, `${i + 35}`));
+}
+
+persons.forEach((person) => console.log(printPerson(person)));
